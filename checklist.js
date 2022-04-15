85eb4a669e191498ba9a37b0a43f5599ecac406e
paulozemr <102665914+paulozemr@users.noreply.github.com>
2022-04-15 15:50:18 -0300

    first commit

---
+++ checklist.js
@@ -0,0 +1,49 @@
+let listaCompras = [
+
+]
+
+function alternarItem(item) {
+    const valor = listaCompras.find((i) => i === item)
+    valor.removido = !valor.removido
+    renderListaCompras()
+}
+
+function addItemUL(item) {
+    const inputItem = document.createElement('input')
+    inputItem.setAttribute('type', 'checkbox')
+    inputItem.addEventListener('click', () => alternarItem(item))
+
+    const labelItem = document.createElement('label')
+    labelItem.appendChild(inputItem)
+    labelItem.appendChild(document.createTextNode(item.item))
+
+    const itemLista = document.createElement('li')
+    itemLista.appendChild(labelItem)
+    if (item.removido) {
+        itemLista.setAttribute('class', 'removed')
+        inputItem.setAttribute('checked', true)
+    }
+
+    const lista = document.getElementById('lista-compra')
+    lista.appendChild(itemLista)
+}
+
+function renderListaCompras() {
+    document.getElementById('lista-compra').innerHTML = ''
+
+    for (const item of listaCompras) {
+        addItemUL(item)
+    }
+}
+
+
+function addItem() {
+    const newItemText = document.getElementById('new-item').value
+    if (newItemText != '') {
+        const newItem = { id: 1, item: newItemText }
+        listaCompras.push(newItem)
+        addItemUL(newItem)
+    }
+}
+
+renderListaCompras()
